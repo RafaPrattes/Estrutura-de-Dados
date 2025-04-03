e2850f62327117d14f218316c28f7d605889ec8a
Rafaela Prates <142021465+RafaPrattes@users.noreply.github.com>
2025-04-03 17:01:42 -0300

    produto.h

--- modularizacao/produto.h
+++ modularizacao/produto.h
@@ -1,16 +1,17 @@
 #ifndef PRODUTO_H
 #define PRODUTO_H
-
+//Definindo a estrutura Produto, que registra um item a ser vendido
 typedef struct
 {
     int codigo;
     char nome[30];
     float preco;
 } Produto;
-
+//Declara as funções que lidam com Produto, mas ainda não as implementa
 void infoProduto(Produto prod);
 void cadastrarProduto();
 void listarProdutos();
 Produto pegarProdutoPorCodigo(int codigo);
 
 #endif
+//ifndef, define e endif impedem a inclusão múltipla que causam erros de redefinição

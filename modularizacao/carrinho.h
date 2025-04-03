01dbe6f2c7283f52a5b2546c30f4ea1afbdfd665
Rafaela Prates <142021465+RafaPrattes@users.noreply.github.com>
2025-04-03 17:26:25 -0300

    carrinho.h

--- modularizacao/carrinho.h
+++ modularizacao/carrinho.h
@@ -1,17 +1,18 @@
-#ifndef CARRINHO_H
-#define CARRINHO_H
+#ifndef CARRINHO_H //--> SE o Carrinho não foi definido ainda
+#define CARRINHO_H //Ele será definido
 
 #include "produto.h"
-
+//Definição da estrutura Carrinho que representa os produtos adicionados e sua quantidade
 typedef struct
 {
     Produto produto;
     int quantidade;
 } Carrinho;
 
+//Funções declaradas para manipulação do carrinho
 void comprarProduto();
 void visualizarCarrinho();
 int *temNoCarrinho(int codigo);
 void fecharPedido();
 
-#endif
+#endif //--> Fecha a verificação da duplicidade

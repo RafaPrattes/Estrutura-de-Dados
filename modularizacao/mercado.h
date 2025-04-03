2283399f8bd5bf2b810a74decf58c0bed3f168e3
Rafaela Prates <142021465+RafaPrattes@users.noreply.github.com>
2025-04-03 16:49:15 -0300

    mercado.h

--- modularizacao/mercado.h
+++ modularizacao/mercado.h
@@ -1,4 +1,5 @@
-#ifndef MERCADO_H
+//Esse arquivo .h funciona como um cabeçalho geral
+#ifndef MERCADO_H //-->ifndef e define evitam repetir inclusões assim como endif
 #define MERCADO_H
 
 #include <stdio.h>
@@ -6,9 +7,12 @@
 #include <string.h>
 #include <unistd.h>
 
+//Incluindo as bibiliotecas necessárias e os headers específicos
 #include "produto.h"
 #include "carrinho.h"
 
+//Declarando a função menu para ser usada em outros arquivos
 void menu();
 
+//Evita inclusão múltipla
 #endif
